32157e94bf8e288b3a5cdc55d3b6ffc97c422dea
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-08-28 21:20:26 -0500

    Agregar archivos de proyecto.

---
+++ Problema 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos,minuto1,horas;
+            Console.WriteLine("Ingrese la cantidad de minutos: ");
+            minutos = int.Parse(Console.ReadLine());
+            horas = minutos / 60;
+            minuto1 =horas % 60;
+            Console.WriteLine("A continuacion mostraremos las horas y minutos que quedaron");
+            Console.WriteLine($"{minutos} minutos son {horas} horas y {minuto1} minutos.");
+            Console.ReadKey();
+        }
+    }
+}
